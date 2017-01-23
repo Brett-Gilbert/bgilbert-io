e752cbe095c36fe4e2a2a110004fa04823a407aa
Brett Gilbert <bgilb13@gmail.com>
2017-01-22 21:45:34 -0700

    Working on content

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,12 +5,20 @@
 		<title>PWP Milestone 1</title>
 	</head>
 	<body>
-		<h1>Purpose, Audience, Goal</h1>
-		<h2>Purpose</h2>
+		<h1>PWP Milestone 1</h1>
+		<h2>Purpose, Audience, Goal</h2>
+		<h3>Purpose</h3>
 		<p>This site is a professional portfolio meant to demonstrate primarily front-end web development.</p>
-		<h2>Audience</h2>
+		<h3>Audience</h3>
 		<p>This site is designed for a professional audience with a particular emphasis on potential employers. This target audience could be comprised of peers, hiring managers, startup managers, or recruiters.</p>
-		<h2>Goal</h2>
+		<h3>Goal</h3>
 		<p>The primary goal of this site is to demonstrate my abilities as a fullstack web developer. The secondary goal for this site is to act as a contact point for potential employers.</p>
+		<h2>Persona</h2>
+		<p>Name: Russell Miller</p>
+		<p>Age: 43</p>
+		<p>Profession: Russell is the hiring manager for FreshFlash. FreshFlash is a small, but rapidly growing startup specializing in web-based grocery ordering and delivery. He is looking for a full time, entry level, web developer to help maintain and expand the already rapidly growing website.</p>
+		<p>Technology: Top of the line System 76 laptop running Ubuntu 16.04, Google Pixel running Android Marshmallow.</p>
+		<p>Needs: As the newly promoted hiring manager of a rapidly growing  startup, Russell has found himself trying to look over resumes in every minute of spare time he has.</p>
+		<p>Goals: Russell's goal is to quickly find standout candidates to fill job openings. When he notices bgilbert.io on the top of my resume, He knows it will be a quick way to gauge how talented of a web developer </p>
 	</body>
 </html>

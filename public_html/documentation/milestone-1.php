c16d859c09f91cd3a41d921ec4c20c60af6391e9
Brett Gilbert <bgilb13@gmail.com>
2017-01-23 00:30:07 -0700

    finished use case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -18,7 +18,9 @@
 		<p>Age: 43</p>
 		<p>Profession: Russell is the hiring manager for FreshFlash. FreshFlash is a small, but rapidly growing startup specializing in web-based grocery ordering and delivery. He is looking for a full time, entry level, web developer to help maintain and expand the already rapidly growing website.</p>
 		<p>Technology: Top of the line System 76 laptop running Ubuntu 16.04, Google Pixel running Android Marshmallow.</p>
-		<p>Needs: As the newly promoted hiring manager of a rapidly growing  startup, Russell has found himself trying to look over resumes in every minute of spare time he has.</p>
-		<p>Goals: Russell's goal is to quickly find standout candidates to fill job openings. When he notices bgilbert.io on the top of my resume, He knows it will be a quick way to gauge how talented of a web developer </p>
+		<p>Needs: As the newly promoted hiring manager of a rapidly growing  startup, Russell has found himself trying to look over resumes in every minute of spare time he has. Anything that could speed up his decision making process about a candidate is what he needs.</p>
+		<p>Goals: Russell's goal is to quickly find standout candidates to fill job openings. If he can determine whether a candidate is worthy of a callback in 3 minutes or less, he is happy.</p>
+		<h2>Use Case</h2>
+		Russell has a new batch of resumes on his desk. after his preliminary weed-out, there are 8 remaining. he has quickly glanced at all of them and is confident that these 8 candidates are somewhat compitent. He finishes up writing some emails, and has about 15 minutes before a lunch meeting. He begins thumbing through the emails and notices my personal website on the heading of my cover letter. He has 15 minutes to review 8 personal websites and determine which are worthy of a callback.
 	</body>
 </html>

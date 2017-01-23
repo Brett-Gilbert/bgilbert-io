58b35097e7dd61879846abdcd4051d98b8cd9e27
Brett Gilbert <bgilb13@gmail.com>
2017-01-22 18:40:11 -0700

    added purpose, audience & goal

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>PWP Milestone 1</title>
+	</head>
+	<body>
+		<h1>Purpose, Audience, Goal</h1>
+		<h2>Purpose</h2>
+		<p>This site is a professional portfolio meant to demonstrate primarily front-end web development.</p>
+		<h2>Audience</h2>
+		<p>This site is designed for a professional audience with a particular emphasis on potential employers. This target audience could be comprised of peers, hiring managers, startup managers, or recruiters.</p>
+		<h2>Goal</h2>
+		<p>The primary goal of this site is to demonstrate my abilities as a fullstack web developer. The secondary goal for this site is to act as a contact point for potential employers.</p>
+	</body>
+</html>

6f93a62b8e292dda3bec41c14393ddf4ef4f43ee
Brett Gilbert <bgilb13@gmail.com>
2017-02-05 22:25:05 -0700

    working on milestone-2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>PWP Milestone 2</title>
+	</head>
+	<body>
+		<h1>PWP Milestone 2</h1>
+		<h2>Content Strategy</h2>
+		<p>My site will be a single page and consist of the following sections</p>
+		<h3>Welcome</h3>
+		<p>This will be a brief, 1 line message welcoming visitors to my website and explaining its purpose. It will be overlaid on a picture of the Sandia Mountains or something like that which will be the persistent background for the entire page.</p>
+		<h3>About Me</h3>
+		<p>This section will contain a photo of me and a couple of paragraphs about who I am.</p>
+		<h3>Portfolio</h3>
+		<p>This section will contain links in the form of screenshots to some of my work and include a short blurb about each site.</p>
+		<h3>Contact Me</h3>
+		<p>This section will contain a short form that can be filled out in order to contact me via email. It will also contain a Linkedin icon at the bottom that will redirect the user to my Linkedin profile.</p>
+		<h3>Mockups</h3>
+		<img src="../images/browser-mockup.png"
+	</body>
+</html>
